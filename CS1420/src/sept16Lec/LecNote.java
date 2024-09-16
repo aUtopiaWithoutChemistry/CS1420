6c6820306330610ea3bd163ac6785f7e6e9f2801
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-09-16 12:33:22 -0600

    added sep 16 lecnote

--- CS1420/src/sept16Lec/LecNote.java
+++ CS1420/src/sept16Lec/LecNote.java
@@ -1,2 +1,26 @@
-package sept16Lec;public class LecNote {
+package sept16Lec;
+
+/**
+ * @author Zifan Zuo
+ * @version Sep 16, 2024
+ */
+public class LecNote {
+    public static void main (String args[]) {
+        // two-dimentional arrays
+        // double[3][2] three is number of rows, two is columns
+        double[][] matrix = new double[3][2];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = 0.0;
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
